3fb94a528129508e1d9e73065226bf63b3f53384
donald mcgrath <don@mcgrathdesigns.com>
2016-01-29 11:34:35 -0500

    added some samples, changed bg color, added interaction with footer links

--- js/application.js
+++ js/application.js
@@ -12,35 +12,67 @@ window.onload = function() {
     
     $('footer').on('click', 'a', function(e){
         e.preventDefault();
-        var self = $(this),contactLabel,spanElement,text,username = "dondmcg";
+        var self = $(this),contactLabel,spanElement,text,username = "dondmcg",myemail = "don@mcgrathdesigns.com",mailhref,telhref;
         targetElement = self.next('.contact-target');
         contactLabel = self.find('.contact-label').html();
         switch(contactLabel) {
             case 'email':
-                text = ' <a href="mailto:Don@mcgrathdesigns.com?Subject=From%20the%20Website%20of%20Donald%20McGrath" target="new">Don@mcgrathdesigns.com</a>';
+                text = ' <a id="myemaillink" href="#" target="new"">Don@mcgrathdesigns.com</a>';
                 break;
             case 'cell phone':
-                text = ' (text enabled) (646) 734-8448';
+                text = ' (text enabled) <a id="myphonelink" href="#">646-734-8448</a>';
                 break;
             case 'mailing address':
                 text = ' Cutchogue N.Y. 11935';
                 break;
-            case 'google plus':
+            case 'google hangout':
                 text = ' add me using dondmcg@gmail';
                 break;
-            case 'aim':
-                text = ' username: ' + username;
+            case 'sms/imessage':
+                text = ' contact me using email: ' + myemail;
                 break;
             case 'skype':
                 text = ' username: ' + username;
                 break;
             case 'screenhero':
-                text = ' username: ' + username;
+                text = ' username: ' + myemail;
                 break;
             default:
                 text = 'This element has not been assigned a value';
         };
         targetElement.html(targetElement.html() === text ? '' : text )
+    });
+    $(document).on("click", "#myemaillink", function(e){ 
+        e.preventDefault();
+        window.location='mailto:Don@mcgrathdesigns.com?Subject=From%20the%20Website%20of%20Donald%20McGrath';
+        return false;
+    });
+    $(document).on("click", "#myphonelink", function(e){ 
+        e.preventDefault();
+        window.location='tel:646-734-8448';
+        return false;
+    });
+    $(document).on("click", "#mychatlink", function(e){ 
+        e.preventDefault();
+        var usr;
+        usr = navigator.platform.match(/(Mac)/i)?'imessage':'sms';
+        window.location= usr + ':16467348448';
+        return false;
+    });
+    $(document).on("click", "#myskypelink", function(e){ 
+        e.preventDefault();
+        window.location='skype:dondmcg?call';
+        return false;
+    });
+    // open external html demo modal
+    $('#demomodal').on("show.bs.modal", function(e){ 
+        var link = $(e.relatedTarget), self=$(this), description;
+        description = link.attr('data-description');
+        self.find(".modal-body").load(link.attr("href"));
+        self.find(".modal-title").html(link.attr("data-title"));
+        if(description){
+            self.find(".modal-title").after("<h3 class='modal-description'>" + description + "</h3>");
+        }
     });
 };
 //jQuery for page scrolling feature - requires jQuery Easing plugin
